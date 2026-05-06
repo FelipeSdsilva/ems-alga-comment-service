694244332652a47b494267cbdf4b16f6ab9965b2
Felipe Sousa <felipe.fps09@hotmail.com>
2026-05-06 12:09:12 -0300

    Feat: Start project

---
+++ src/main/java/com/felipesouls/alga_comment_service/AlgaCommentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.felipesouls.alga_comment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaCommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaCommentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesouls/alga_comment_service/AlgaCommentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesouls.alga_comment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaCommentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
