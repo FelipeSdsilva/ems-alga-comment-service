881568aadc7f6d82f622157053e30d626e4a93b2
Felipe Sousa <felipe.fps09@hotmail.com>
2026-05-12 08:26:34 -0300

    Feat: Adjusting in class name.

--- src/main/java/com/felipesouls/alga_comment_service/api/controllers/impl/CommentControllerImpl.java
+++ src/main/java/com/felipesouls/alga_comment_service/api/controllers/impl/CommentControllerImpl.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/comments")
-public class CommandControllerImpl implements CommentController {
+public class CommentControllerImpl implements CommentController {
 
   private final CommentService commentService;
 
